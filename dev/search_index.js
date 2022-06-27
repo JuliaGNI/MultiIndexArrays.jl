4411a2abf5d8807701cc66c0ff0e5d6e8137d8fe
Documenter.jl <documenter@juliadocs.github.io>
2022-06-27 09:00:35 +0000

    build based on 0a60e40

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MultiIndexArrays","category":"page"},{"location":"#MultiIndexArrays","page":"Home","title":"MultiIndexArrays","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MultiIndexArrays.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MultiIndexArrays]","category":"page"}]
+}
